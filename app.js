b5b98063f6b24b0bd31a82a449f8ffec461ede55
kalanka@zindhu.lk <kalanka@zindhu.lk>
2025-03-28 14:02:23 +0530

    jwt token generation and verification added

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const bodyParser = require('body-parser')
+const signJwtRoute = require('./routes/signJwt')
+
+app.use(bodyParser.json())
+
+app.use('/', signJwtRoute)
+
+const port = 8000
+app.listen(port, () => {
+  console.log(`server is running on http://localhost:${port}`)
+})
